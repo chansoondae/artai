961510050cb1d95a1a0299010d5bfbc28168b690
Chansu Suh <chansusuh@Chansuui-noteubug.local>
2024-11-04 08:20:42 +0900

    firebase integration

--- app/firebaseConfig.js
+++ app/firebaseConfig.js
@@ -12,19 +12,17 @@ import { signOut } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBHOX2AcUYNlLka7nMQBMjvdq4LbgDH8ZQ",
-    authDomain: "artai-c1229.firebaseapp.com",
-    projectId: "artai-c1229",
-    storageBucket: "artai-c1229.firebasestorage.app",
-    messagingSenderId: "476230734880",
-    appId: "1:476230734880:web:023887790af90543bd1845",
-    measurementId: "G-F2V1P57Z4R"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID,
+    appId: process.NEXT_PUBLIC_FIREBASE_APPID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENTID,
   };
   
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics =
-  app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
 
 export default app;
 
