04f72726c2a35be33d2fabde63454095116c49eb
chansoondae <info@mongtravel.net>
2024-11-05 20:25:52 +0900

    chatGPT error fixed

--- pages/api/chatGPT.js
+++ pages/api/chatGPT.js
@@ -1,5 +1,3 @@
-// pages/api/chatGPT.js
-
 import OpenAI from "openai";
 
 const openai = new OpenAI({
@@ -11,38 +9,36 @@ export default async function handler(req, res) {
     return res.status(405).json({ message: 'Only POST requests are allowed' });
   }
 
-  const { question, artistName, artworkTitle } = req.body;
-  const { messages } = req.body; // 이전 메시지 히스토리를 클라이언트에서 받습니다.
+  const { question, artistName, artworkTitle, messages = [] } = req.body;
 
   try {
+    // 기존 대화 히스토리를 포함하여 새 메시지 생성
     const completion = await openai.chat.completions.create({
       model: "gpt-4o-mini",
       messages: [
         { 
           role: "system", 
-          content: "You are a knowledgeable exhibition docent. Provide brief responses in Korean, aiming for 3 to 10 sentences. Then, Provide exactly three related questions as examples.Please mark '++##++'' between the answer and the expected question and make 3 questions with <1>, <2>, and <3>." 
+          content: "You are a knowledgeable exhibition docent. Provide brief responses in Korean, aiming for 3 to 10 sentences. Then, provide exactly three related questions as examples. Please mark '++##++' between the answer and the expected questions and make 3 questions with <1>, <2>, and <3>." 
         },
         { 
           role: "user", 
-          content: `작가: ${artistName}, 작품: ${artworkTitle}에 대한 작품 설명 부탁드립니다. ${question}`
+          content: `작가: ${artistName}, 작품: ${artworkTitle}에 대한 작품 설명 부탁드립니다. ${question}` 
         },
-        ...messages, // 기존 대화 히스토리를 포함합니다.
+        ...messages, // 기존 대화 히스토리를 추가합니다.
       ],
       max_tokens: 500,
       temperature: 0.7,
     });
-    
 
     const responseText = completion.choices[0]?.message?.content.trim();
 
     // ChatGPT 응답을 파싱하여 답변과 질문 예시 3개를 추출
-    // const [answer, ...questionExamples] = responseText.split("\n").filter(Boolean);
     const [answer, rawQuestions] = responseText.split("++##++").map(text => text.trim());
 
     // Process rawQuestions by splitting based on <1>, <2>, <3> markers to form the array
     const questionExamples = rawQuestions
-    ? rawQuestions.split(/<\d+>/).filter(Boolean).map(question => question.trim())
-    : []; // If rawQuestions is empty or undefined, default to an empty array
+      ? rawQuestions.split(/<\d+>/).filter(Boolean).map(question => question.trim())
+      : []; // If rawQuestions is empty or undefined, default to an empty array
 
     res.status(200).json({ answer, questionExamples });
   } catch (error) {
